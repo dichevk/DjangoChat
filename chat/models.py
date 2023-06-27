d4659b16d8938358cc29b0bbf82d3b699cd8f713
KonstantinDichev <kdichev.nl@gmail.com>
2023-06-27 23:55:06 +0200

    message class and folder structure updates

---
+++ chat/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from account.models import User
+
+class Message(models.Model):
+    body = models.TextField()
+    sent_by = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL)
+
+    class Meta: 
+        ordering = ('created_at')
+
+    def __str__(self):
+        return f'{self.sent_by}'
