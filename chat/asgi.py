02b082683f8b6b295b22730fbc45f835da25f3a6
KonstantinDichev <kdichev.nl@gmail.com>
2023-06-30 23:17:48 +0200

    app asgi connection

--- chat/asgi.py
+++ chat/asgi.py
@@ -3,13 +3,14 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import URLResolver, ProtocolTypeRouter
+from app import routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
 
 asgi_application = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': asgi_application,
-    'websocket': AllowedHostsOriginValidator(AuthMiddlewareStack(URLResolver()))
+    'websocket': AllowedHostsOriginValidator(AuthMiddlewareStack(URLResolver(routing.websocket_urlpatterns)))
 
 })
 
---
+++ chat/tests.py
@@ -0,0 +1,3 @@
+from django.test import TestCase
+
+''' setup unit tests for the module here'''
