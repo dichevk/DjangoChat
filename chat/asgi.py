ce4c063ee2eda82230e9a094378fc2ee740844cb
KonstantinDichev <kdichev.nl@gmail.com>
2023-06-30 23:00:06 +0200

    app module and chat module updates

---
+++ chat/asgi.py
@@ -0,0 +1,15 @@
+import os 
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import URLResolver, ProtocolTypeRouter
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
+
+asgi_application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': asgi_application,
+    'websocket': AllowedHostsOriginValidator(AuthMiddlewareStack(URLResolver()))
+
+})
+
