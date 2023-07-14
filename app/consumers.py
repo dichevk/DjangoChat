466a2a989f5b31045ee9e013e278b57ab39b57c2
KonstantinDichev <kdichev.nl@gmail.com>
2023-07-14 11:21:24 +0200

    receive function implemented

--- app/consumers.py
+++ app/consumers.py
@@ -3,6 +3,8 @@ import json
 from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import Room, Message
+from django.utils.timesince import timesince
+from .templates.app.components.template_tags import initials
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -17,6 +19,43 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, code):
         return await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
     
+    async def receive(self, text_data):
+        # Receive message from WebSocket (front end)
+        text_data_json = json.loads(text_data)
+        type = text_data_json['type']
+        message = text_data_json['message']
+        name = text_data_json['name']
+        agent = text_data_json.get('agent', '')
+    
+        
+        print('Receive:', type)
+
+        if type == 'message':
+            new_message = await self.create_message(name, message, agent)
+
+            # Send message to group / room
+            await self.channel_layer.group_send(
+                self.room_group_name, {
+                    'type': 'chat_message',
+                    'message': message,
+                    'name': name,
+                    'agent': agent,
+                    'initials': initials(name),
+                    'created_at': timesince(new_message.created_at),
+                }
+            )
+        elif type == 'update':
+            print('is update')
+            # Send update to the room
+            await self.channel_layer.group_send(
+                self.room_group_name, {
+                    'type': 'writing_active',
+                    'message': message,
+                    'name': name,
+                    'agent': agent,
+                    'initials': initials(name),
+                }
+            )
 
     @sync_to_async
     def get_room(self):
