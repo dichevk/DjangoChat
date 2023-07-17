5bb82698baed9d79574b2b373472bb629fbc156a
KonstantinDichev <kdichev.nl@gmail.com>
2023-07-17 23:27:04 +0200

    create room post endpoint implemented

--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
+from .models import Room
+from django.http import JsonResponse
+
 @require_POST
 def create_room(request, uuid):
-    pass
+    name = request.POST.get('name', '')
+    url = request.POST.get('url', '')
+    Room.objects.create(uuid=uuid, client=name, url=url)
+    return JsonResponse({'message': 'room created'})
+
+
 @login_required
 def admin(request):
     pass
