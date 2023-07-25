4b1e57ddfd410cac01a679ac120d191257cacb6c
KonstantinDichev <kdichev.nl@gmail.com>
2023-07-25 23:57:07 +0200

    test improvements

--- app/tests.py
+++ app/tests.py
@@ -2,9 +2,41 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 from app.views import edit_user, add_user
+from account.forms import EditUserForm, AddUserForm
 
 
 class EditUserTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user_with_permission = User.objects.create_user(username='user_with_perm', password='testpassword')
+        self.user_with_permission.user_permissions.add('user.edit_user')
+        self.user_without_permission = User.objects.create_user(username='user_without_perm', password='testpassword')
+        self.user_to_edit = User.objects.create_user(username='user_to_edit', password='testpassword')
+
+    def test_edit_user_with_permission(self):
+        self.client.login(username='user_with_perm', password='testpassword')
+        form_data = {'username': 'new_username', 'email': 'new_email@example.com'}
+        response = self.client.post(reverse('edit_user', args=[self.user_to_edit.pk]), data=form_data)
+        self.assertEqual(response.status_code, 302)  # 302 is the redirect status code
+        self.assertEqual(response.url, '/chat-admin/')
+        self.assertEqual(User.objects.get(pk=self.user_to_edit.pk).username, 'new_username')
+        self.assertEqual(User.objects.get(pk=self.user_to_edit.pk).email, 'new_email@example.com')
+
+    def test_edit_user_without_permission(self):
+        self.client.login(username='user_without_perm', password='testpassword')
+        form_data = {'username': 'new_username', 'email': 'new_email@example.com'}
+        response = self.client.post(reverse('edit_user', args=[self.user_to_edit.pk]), data=form_data)
+        self.assertEqual(response.status_code, 302)  # 302 is the redirect status code
+        self.assertEqual(response.url, '/chat-admin/')
+        self.assertEqual(User.objects.get(pk=self.user_to_edit.pk).username, 'user_to_edit')  # No change
+        self.assertEqual(User.objects.get(pk=self.user_to_edit.pk).email, '')  # No change
+
+    def test_edit_user_permission_denied(self):
+        response = self.client.post(reverse('edit_user', args=[self.user_to_edit.pk]), data={})
+        self.assertEqual(response.status_code, 302)  # 302 is the redirect status code
+        self.assertEqual(response.url, '/chat-admin/')
+        self.assertIn(b"You don't have access to edit users!", response.content)
+
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user(username='testuser', password='testpassword')
@@ -28,19 +60,36 @@ class EditUserTestCase(TestCase):
 class AddUserTestCase(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.user.user_permissions.add('user.add_user')
+        self.user_with_permission = User.objects.create_user(username='user_with_perm', password='testpassword')
+        self.user_with_permission.user_permissions.add('user.add_user')
+        self.user_without_permission = User.objects.create_user(username='user_without_perm', password='testpassword')
 
-    def test_add_user_success(self):
-        self.client.login(username='testuser', password='testpassword')
-        response = self.client.post(reverse('add_user'), data={'some_form_data': 'value'})
+    def test_add_user_with_permission(self):
+        self.client.login(username='user_with_perm', password='testpassword')
+        form_data = {'username': 'new_user', 'email': 'new_user@example.com', 'password': 'password123'}
+        response = self.client.post(reverse('add_user'), data=form_data)
         self.assertEqual(response.status_code, 302)  # 302 is the redirect status code
         self.assertEqual(response.url, '/app-admin/')
-        self.assertEqual(User.objects.count(), 2)  # Assuming the user count before the test is 1
+        new_user = User.objects.get(username='new_user')
+        self.assertTrue(new_user.is_staff)
+        self.assertTrue(new_user.check_password('password123'))
+
+    def test_add_user_without_permission(self):
+        self.client.login(username='user_without_perm', password='testpassword')
+        form_data = {'username': 'new_user', 'email': 'new_user@example.com', 'password': 'password123'}
+        response = self.client.post(reverse('add_user'), data=form_data)
+        self.assertEqual(response.status_code, 302)  # 302 is the redirect status code
+        self.assertEqual(response.url, '/app-admin/')
+        self.assertIsNone(User.objects.filter(username='new_user').first())
+
+    def test_add_user_permission_denied(self):
+        response = self.client.post(reverse('add_user'), data={})
+        self.assertEqual(response.status_code, 302)  # 302 is the redirect status code
+        self.assertEqual(response.url, '/app-admin/')
+        self.assertIn(b"Access denied", response.content)
 
     def test_add_user_permission_denied(self):
         self.client.login(username='testuser', password='testpassword')
-        self.user.user_permissions.remove('user.add_user')
         response = self.client.post(reverse('add_user'), data={'some_form_data': 'value'})
         self.assertEqual(response.status_code, 302)  # 302 is the redirect status code
         self.assertEqual(response.url, '/app-admin/')
