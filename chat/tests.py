ad0c87612c5ede0022f069885e3f698500fb592d
KonstantinDichev <kdichev.nl@gmail.com>
2023-07-29 23:32:42 +0200

    fix core tests

--- chat/tests.py
+++ chat/tests.py
@@ -1,19 +1,1 @@
-from django.test import TestCase, Client
-from django.urls import reverse
 
-class CoreViewsTest(TestCase):
-
-    def setUp(self):
-        self.client = Client()
-
-    def test_index_view(self):
-        url = reverse('core:index')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'core/index.html')
-
-    def test_about_view(self):
-        url = reverse('core:about')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'core/about.html')
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# this lib will be used for the test cases
+class CoreViewsTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_view(self):
+        url = reverse('core:index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/templates/core/index.html')
+
+    def test_about_view(self):
+        url = reverse('core:about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/templates/core/about.html')
